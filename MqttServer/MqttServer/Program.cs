899e683a5028355eb20366855389685e5f80c093
Every <Every_126@outlook.com>
2019-01-08 00:49:45 +0800

    Add MqttServer file

---
+++ MqttServer/MqttServer/Program.cs
@@ -0,0 +1,92 @@
+﻿using MQTTnet;
+using MQTTnet.Protocol;
+using MQTTnet.Server;
+using System;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace mqttServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyWriteLine("Start running mqtt server......");
+            Task.Run(RunServerTaskAsync);
+            MyWriteLine("Press anykey to exit.....");
+            Console.ReadKey();
+        }
+
+        public static async Task RunServerTaskAsync()
+        {
+            var mqttServer = new MqttFactory().CreateMqttServer();
+
+            IPAddress iPAddress = IPAddress.Parse("127.0.0.1");
+            var optionBuilder = new MqttServerOptionsBuilder()
+                .WithConnectionBacklog(100)
+                .WithDefaultEndpointBoundIPAddress(iPAddress)
+                .WithDefaultEndpointPort(1883);
+
+            var options = optionBuilder.Build();
+            (options as MqttServerOptions).ConnectionValidator = c =>
+            {
+                if (c.ClientId.Length < 10)
+                {
+                    c.ReturnCode = MqttConnectReturnCode.ConnectionRefusedIdentifierRejected;
+                    return;
+                }
+
+                if (c.Username != "user")
+                {
+                    c.ReturnCode = MqttConnectReturnCode.ConnectionRefusedBadUsernameOrPassword;
+                    return;
+                }
+
+                if (c.Password != "password")
+                {
+                    c.ReturnCode = MqttConnectReturnCode.ConnectionRefusedBadUsernameOrPassword;
+                    return;
+                }
+
+                c.ReturnCode = MqttConnectReturnCode.ConnectionAccepted;
+            };
+
+            MyWriteLine("Mqttsever is Starting.....");
+            await mqttServer.StartAsync(options);
+
+            mqttServer.ClientConnected += (s, e) =>
+            {
+                MyWriteLine($"{e.ClientId} has connected!");
+            };
+
+            mqttServer.ClientDisconnected += (s, e) =>
+            {
+                MyWriteLine($"{e.ClientId} has ClientDisconnected");
+            };
+
+            mqttServer.Started += (s, e) =>
+            {
+                MyWriteLine("The mqtt server has started!");
+
+            };
+
+            mqttServer.Stopped += (s, e) =>
+            {
+                MyWriteLine("The mqttserver has stopped!");
+            };
+
+            
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadLine();
+            await mqttServer.StopAsync();
+
+            
+        }
+
+        public static void MyWriteLine(string msg)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine(msg);
+        }
+    }
+}
